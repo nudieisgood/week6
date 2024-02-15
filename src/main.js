3cf2b45cfa2fe6f42d309011b5d7dffef8de02e0
jeremychan523 <jeremychan523@wonder4studio.com>
2024-02-15 22:10:38 +0800

    ready

--- src/main.js
+++ src/main.js
@@ -1,14 +1,34 @@
 import './assets/main.css'
 
+// import axios from 'axios'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+import { Form, Field, ErrorMessage, defineRule, configure } from 'vee-validate'
+import * as AllRules from '@vee-validate/rules'
+import { localize, setLocale } from '@vee-validate/i18n'
+import zhTW from '@vee-validate/i18n/dist/locale/zh_TW.json'
+
 import App from './App.vue'
 import router from './router'
+
+Object.keys(AllRules).forEach((rule) => {
+  defineRule(rule, AllRules[rule])
+})
+
+configure({
+  generateMessage: localize({ zh_TW: zhTW }), // 載入繁體中文語系
+  validateOnInput: true // 當輸入任何內容直接進行驗證
+})
+
+// 設定預設語系
+setLocale('zh_TW')
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-
+app.component('VeeForm', Form)
+app.component('VeeField', Field)
+app.component('ErrorMessage', ErrorMessage)
 app.mount('#app')
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,50 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
+import HomeLayout from '@/views/HomeLayout'
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: () => import('../views/HomeView.vue')
+    name: 'HomeLayout',
+    component: HomeLayout,
+    children: [
+      { path: '', name: 'Home', component: () => import('@/views/HomeView') },
+      {
+        path: 'products',
+        name: 'Products',
+        component: () => import('@/views/ProductsView')
+      },
+
+      {
+        path: 'cart',
+        name: 'Cart',
+        component: () => import('@/views/CartView')
+      },
+      {
+        path: 'login',
+        name: 'Login',
+        component: () => import('@/views/LoginView')
+      },
+      {
+        path: 'product/:id',
+        name: 'Product',
+        component: () => import('@/views/ProductView')
+      }
+    ]
   },
   {
-    path: '/about',
-    name: 'About',
-    component: () => import('../views/AboutView.vue')
+    path: '/admin',
+    name: 'adminLayout',
+    component: () => import('@/views/AdminLayout'),
+    children: [
+      {
+        path: 'products',
+        component: () => import('@/views/AdminProducts')
+      },
+      {
+        path: 'orders',
+        component: () => import('@/views/AdminOrders')
+      }
+    ]
   }
 ]
 
